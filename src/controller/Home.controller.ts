4e98e6a37ef5eca4ac030d1c652844a28bb14d76
zhixiong <lxsbw@outlook.com>
2021-03-08 23:18:20 +0800

    add linqts

--- src/controller/Home.controller.ts
+++ src/controller/Home.controller.ts
@@ -5,11 +5,23 @@
  * @Last Modified time: 2021-02-21 15:57:41
  */
 import { Controller, Get } from '@nestjs/common';
+import { List } from 'linqts';
 
 @Controller()
 export class HomeController {
   @Get('/')
   async home() {
     return 'Hello Nestjs!';
+  }
+
+  @Get('/linq')
+  async linq() {
+    const result = new List<number>([1, 2, 3, 4, 5])
+      .Where((x) => x > 3)
+      .Select((y) => y * 2)
+      .OrderByDescending((x) => x)
+      .ToArray(); // > [8, 10]
+
+    return result;
   }
 }
