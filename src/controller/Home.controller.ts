ce08194203ec859313e1f7da761c8ba548108780
zhixiong <lxsbw@outlook.com>
2021-03-09 11:04:33 +0800

    linq ThenBy

--- src/controller/Home.controller.ts
+++ src/controller/Home.controller.ts
@@ -16,11 +16,32 @@ export class HomeController {
 
   @Get('/linq')
   async linq() {
-    const result = new List<number>([1, 2, 3, 4, 5])
-      .Where((x) => x > 3)
-      .Select((y) => y * 2)
-      .OrderByDescending((x) => x)
-      .ToArray(); // > [8, 10]
+    // const result = new List<number>([1, 2, 3, 4, 5])
+    //   .Where((x) => x > 3)
+    //   .Select((y) => y * 2)
+    //   .OrderByDescending((x) => x)
+    //   .ToArray(); // > [8, 10]
+
+    const data = [
+      {
+        id: 1,
+        name: 'one',
+        category: 'fruits',
+        countries: ['Italy', 'Austria']
+      },
+      {
+        id: 2,
+        name: 'two',
+        category: 'vegetables',
+        countries: ['Italy', 'Germany']
+      },
+      { id: 2, name: 'three', category: 'vegetables', countries: ['Germany'] },
+      { id: 2, name: 'ta', category: 'vegetables', countries: ['Germany'] },
+      { id: 4, name: 'four', category: 'fruits', countries: ['Japan'] },
+      { id: 5, name: 'five', category: 'fruits', countries: ['Japan', 'Italy'] }
+    ];
+
+    const result = new List(data).OrderBy((x) => x.id).ThenBy((x) => x.name);
 
     return result;
   }
